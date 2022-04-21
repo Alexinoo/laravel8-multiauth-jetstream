00cab42af4b7e8ce0f65c3cdb07d6b2d1c849aa3
Alex Mwangi <alex@stl-horizon.com>
2022-04-21 16:00:23 +0300

    Commented on Laravel dashboard area

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,11 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        {{-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-jet-welcome />
             </div>
-        </div>
+        </div> --}}
+        <h3>This is the Dashboard</h3>
     </div>
 </x-app-layout>
